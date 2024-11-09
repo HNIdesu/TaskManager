814a202deec9bdf053452f5bb42683b953f9a049
hnidesu <hnidesu@outlook.com>
2024-11-09 15:51:40 +0800

    refactor: 使用PreferenceFragmentCompat来创建设置界面，替换了自定义的设置界面布局

--- app/src/main/java/com/hnidesu/taskmanager/fragment/SettingFragment.kt
+++ app/src/main/java/com/hnidesu/taskmanager/fragment/SettingFragment.kt
@@ -2,19 +2,15 @@ package com.hnidesu.taskmanager.fragment
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.widget.SwitchCompat
-import androidx.fragment.app.Fragment
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 import com.hnidesu.taskmanager.R
 import com.hnidesu.taskmanager.base.DatabaseTaskSource
-import com.hnidesu.taskmanager.manager.SettingManager.getDefaultSetting
 import com.hnidesu.taskmanager.service.CheckDeadlineService
 import com.hnidesu.taskmanager.util.LogUtil
 import com.hnidesu.taskmanager.util.ToastUtil
@@ -23,123 +19,109 @@ import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
 
-
-class SettingFragment : Fragment() {
-    private val mExportDataLauncher: ActivityResultLauncher<Intent>
-    private val mImportDataLauncher: ActivityResultLauncher<Intent>
-
-    init {
-        mExportDataLauncher= registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult()
-        ) { result ->
-            val context = requireContext()
-            val data = result.data
-            val data2 = data?.data
-            if (result.resultCode == -1 && data2 != null) {
-                val file = context.getDatabasePath("database.db")
+class SettingFragment : PreferenceFragmentCompat() {
+    private val mExportDataLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        val context = requireContext()
+        val data = result.data
+        val data2 = data?.data
+        if (result.resultCode == -1 && data2 != null) {
+            val file = context.getDatabasePath("database.db")
+            try {
+                context.contentResolver.openOutputStream(data2).use { outputStream ->
+                    FileInputStream(file).use { fileInputStream ->
+                        val buffer = ByteArray(65536)
+                        while (true) {
+                            val bytesRead = fileInputStream.read(buffer)
+                            if (bytesRead == -1)
+                                break
+                            outputStream!!.write(buffer, 0, bytesRead)
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                LogUtil.error(getString(R.string.save_data_failed), e)
+                ToastUtil.toastLong(context,R.string.save_data_failed)
+            }
+        } else {
+            ToastUtil.toastShort(context,R.string.cancelled)
+        }
+    }
+    private val mImportDataLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult(),
+        object : ActivityResultCallback<ActivityResult> {
+            override fun onActivityResult(result: ActivityResult) {
+                val context = requireContext()
+                val data=result.data?.data
+                if (result.resultCode != -1 || data == null) {
+                    ToastUtil.toastShort(context,R.string.cancelled)
+                    return
+                }
+                val tempFile = File(context.cacheDir.absolutePath + "/temp_database.db")
+                tempFile.deleteOnExit()
                 try {
-                    context.contentResolver.openOutputStream(data2).use { outputStream ->
-                        FileInputStream(file).use { fileInputStream ->
+                    val inputStream= try {
+                        context.contentResolver.openInputStream(data)
+                    } catch (e: Exception) {
+                        LogUtil.error(context.getString(R.string.import_data_failed), e)
+                        ToastUtil.toastLong(context,R.string.import_data_failed)
+                        return
+                    } ?: throw Exception("读取文件失败")
+                    inputStream.use {
+                        FileOutputStream(tempFile).use { fileOutputStream->
                             val buffer = ByteArray(65536)
                             while (true) {
-                                val bytesRead = fileInputStream.read(buffer)
-                                if (bytesRead == -1)
+                                val bytesRead = it.read(buffer)
+                                if (bytesRead == -1) {
                                     break
-                                outputStream!!.write(buffer, 0, bytesRead)
-                            }
-                        }
-                    }
-                } catch (e: Exception) {
-                    LogUtil.error(getString(R.string.save_data_failed), e)
-                    ToastUtil.toastLong(context,R.string.save_data_failed)
-                }
-            } else {
-                ToastUtil.toastShort(context,R.string.cancelled)
-            }
-        }
-
-        mImportDataLauncher = registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult(),
-            object : ActivityResultCallback<ActivityResult> {
-                override fun onActivityResult(result: ActivityResult) {
-                    val context = requireContext()
-                    val data=result.data?.data
-                    if (result.resultCode != -1 || data == null) {
-                        ToastUtil.toastShort(context,R.string.cancelled)
-                        return
-                    }
-                    val tempFile = File(context.cacheDir.absolutePath + "/temp_database.db")
-                    tempFile.deleteOnExit()
-                    try {
-                        val inputStream= try {
-                            context.contentResolver.openInputStream(data)
-                        } catch (e: Exception) {
-                            LogUtil.error(context.getString(R.string.import_data_failed), e)
-                            ToastUtil.toastLong(context,R.string.import_data_failed)
-                            return
-                        } ?: throw Exception("读取文件失败")
-                        inputStream.use {
-                            FileOutputStream(tempFile).use { fileOutputStream->
-                                val buffer = ByteArray(65536)
-                                while (true) {
-                                    val bytesRead = it.read(buffer)
-                                    if (bytesRead == -1) {
-                                        break
-                                    }
-                                    fileOutputStream.write(buffer, 0, bytesRead)
                                 }
+                                fileOutputStream.write(buffer, 0, bytesRead)
                             }
                         }
-                        val taskList = DatabaseTaskSource(context).getTasks(null).toList()
-                        ImportDataDialog(context, taskList).show()
-                        tempFile.deleteOnExit()
-                    } catch (ex:Exception) {
-                        LogUtil.error(getString(R.string.import_failed),ex)
-                        ToastUtil.toastLong(context,R.string.import_failed)
                     }
+                    val taskList = DatabaseTaskSource(context).getTasks(null).toList()
+                    ImportDataDialog(context, taskList).show()
+                    tempFile.deleteOnExit()
+                } catch (ex:Exception) {
+                    LogUtil.error(getString(R.string.import_failed),ex)
+                    ToastUtil.toastLong(context,R.string.import_failed)
                 }
-            })
-
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val context=requireContext()
-        val rootView = inflater.inflate(R.layout.fragment_setting, container, false)
-        rootView.findViewById<Button>(R.id.btn_export_data).setOnClickListener { view ->
-            val intent = Intent()
-            intent.setAction("android.intent.action.CREATE_DOCUMENT")
-            intent.setType("*/*")
-            intent.addCategory("android.intent.category.OPENABLE")
-            intent.putExtra(
-                "android.intent.extra.TITLE",
-                "database_com.hnidesu.taskmanager.db"
-            )
-            mExportDataLauncher.launch(intent)
-        }
-        rootView.findViewById<Button>(R.id.btn_import_data).setOnClickListener { view ->
-            val intent = Intent()
-            intent.setAction("android.intent.action.PICK")
-            mImportDataLauncher.launch(intent)
-        }
-
-        val view = rootView.findViewById<SwitchCompat>(R.id.enable_task_notification)
-        val sharedPrefs = getDefaultSetting(context)
-        view.setChecked(sharedPrefs.getBoolean("deadline_notification", false))
-        view.setOnCheckedChangeListener { _, checked ->
-            sharedPrefs.edit().putBoolean("deadline_notification", checked).apply()
-            val intent =
-                Intent(context, CheckDeadlineService::class.java)
-            if (checked) {
-                context.startService(intent)
-            } else {
-                context.stopService(intent)
             }
-        }
-        return rootView
-    }
+        })
 
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        preferenceManager.sharedPreferencesName="setting"
+        setPreferencesFromResource(R.xml.preferences,rootKey)
+        preferenceManager.findPreference<Preference>("import_data")?.setOnPreferenceClickListener {
+            val intent = Intent().apply {
+                setAction("android.intent.action.PICK")
+            }
+            mImportDataLauncher.launch(intent)
+            true
+        }
+        preferenceManager.findPreference<Preference>("export_data")?.setOnPreferenceClickListener {
+            val intent = Intent().apply {
+                setAction("android.intent.action.CREATE_DOCUMENT")
+                setType("*/*")
+                addCategory("android.intent.category.OPENABLE")
+                putExtra(
+                    "android.intent.extra.TITLE",
+                    "database_com.hnidesu.taskmanager.db"
+                )
+            }
+
+            mExportDataLauncher.launch(intent)
+            true
+        }
+        preferenceManager.findPreference<SwitchPreference>("deadline_notification")?.setOnPreferenceChangeListener{_,newValue->
+            val intent = Intent(context, CheckDeadlineService::class.java)
+            if (newValue==true)
+                context?.startService(intent)
+            else
+                context?.stopService(intent)
+            true
+        }
+    }
 }

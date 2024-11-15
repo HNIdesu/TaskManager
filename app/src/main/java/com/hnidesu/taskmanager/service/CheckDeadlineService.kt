3d300069e955a0a6f471180218e20004941472f7
hnidesu <hnidesu@outlook.com>
2024-11-16 00:13:26 +0800

    fix: 添加通知权限检查并抑制废弃 API 警告
    - 增加发送通知前的 POST_NOTIFICATIONS 权限检查
    - 对于 API 33 以下的 stopForeground 方法，添加了警告抑制
    - 优化通知渠道初始化代码的格式，提高可读性

--- app/src/main/java/com/hnidesu/taskmanager/service/CheckDeadlineService.kt
+++ app/src/main/java/com/hnidesu/taskmanager/service/CheckDeadlineService.kt
@@ -1,13 +1,16 @@
 package com.hnidesu.taskmanager.service
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.IBinder
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.hnidesu.taskmanager.R
@@ -19,7 +22,6 @@ import com.hnidesu.taskmanager.manager.TaskManager
 import java.util.Timer
 import java.util.TimerTask
 
-
 class CheckDeadlineService : Service() {
     private val mTimer = Timer()
     private val mCheckDeadlineTask = CheckDeadlineTask()
@@ -27,7 +29,7 @@ class CheckDeadlineService : Service() {
     private val mChannelId = "taskmanager"
     private val mForegroundNotificationId = 100
     private val mLastCheckedTasks: Set<Long?> = setOf()
-    @SuppressLint("MissingPermission")
+
     fun sendUrgent(id: Int, msg: String?, intent: Intent?) {
         val builder =
             NotificationCompat.Builder(this, mChannelId).setSmallIcon(R.mipmap.ic_launcher)
@@ -39,7 +41,14 @@ class CheckDeadlineService : Service() {
         if (intent != null)
             builder.setContentIntent(PendingIntent.getActivity(this, 5937, intent, PendingIntent.FLAG_UPDATE_CURRENT))
         val notification = builder.build()
-        NotificationManagerCompat.from(this).notify(id, notification)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            NotificationManagerCompat.from(this).notify(id, notification)
+        }
+
     }
 
     override fun onBind(intent: Intent): IBinder? {
@@ -83,13 +92,13 @@ class CheckDeadlineService : Service() {
         if (Build.VERSION.SDK_INT >= 26) {
             getSystemService(NotificationManager::class.java).createNotificationChannels(
                 listOf(
-                        NotificationChannel(
-                            mForegroundChannelId,
-                            getString(R.string.foreground_service_of_check_deadline_service),
-                            NotificationManager.IMPORTANCE_DEFAULT
-                        ), NotificationChannel(
-                            mChannelId, getString(R.string.upcoming_task_notification), NotificationManager.IMPORTANCE_HIGH
-                        )
+                    NotificationChannel(
+                        mForegroundChannelId,
+                        getString(R.string.foreground_service_of_check_deadline_service),
+                        NotificationManager.IMPORTANCE_DEFAULT
+                    ), NotificationChannel(
+                        mChannelId, getString(R.string.upcoming_task_notification), NotificationManager.IMPORTANCE_HIGH
+                    )
                 )
             )
         }
@@ -103,6 +112,7 @@ class CheckDeadlineService : Service() {
     override fun onDestroy() {
         mTimer.cancel()
         if (Build.VERSION.SDK_INT < 33) {
+            @Suppress("DEPRECATION")
             stopForeground(true)
         } else {
             stopForeground(STOP_FOREGROUND_REMOVE)

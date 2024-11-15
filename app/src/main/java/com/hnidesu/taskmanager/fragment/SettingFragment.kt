ec1509767539f962110e1e12cfc3c9d153d35bfe
hnidesu <hnidesu@outlook.com>
2024-11-15 21:39:06 +0800

    refactor: 使用AlertDialog替代了ImportDataDialog，美化了UI

--- app/src/main/java/com/hnidesu/taskmanager/fragment/SettingFragment.kt
+++ app/src/main/java/com/hnidesu/taskmanager/fragment/SettingFragment.kt
@@ -7,6 +7,7 @@ import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
@@ -16,7 +17,6 @@ import com.hnidesu.taskmanager.manager.TaskManager
 import com.hnidesu.taskmanager.service.CheckDeadlineService
 import com.hnidesu.taskmanager.util.LogUtil
 import com.hnidesu.taskmanager.util.ToastUtil
-import com.hnidesu.taskmanager.widget.dialog.ImportDataDialog
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.BufferedReader
@@ -24,83 +24,105 @@ import java.io.InputStreamReader
 
 class SettingFragment : PreferenceFragmentCompat() {
     private val mExportDataLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
-    ) { result ->
-        val context = requireContext()
-        val data = result.data?.data
-        if (result.resultCode != Activity.RESULT_CANCELED && data != null) {
-            try {
-                context.contentResolver.openOutputStream(data)?.use { outputStream ->
-                    val json=JSONObject()
-                    json.put("version",1)
-                    val taskList= TaskManager.getTasks()
-                    val taskListJsonArray=JSONArray()
-                    json.put("tasks",taskListJsonArray)
-                    for (task in taskList) {
-                        taskListJsonArray.put(JSONObject().apply {
-                            put("create_time", task.createTime)
-                            put("title", task.title)
-                            put("content", task.content)
-                            put("is_finished", task.isFinished)
-                            put("deadline", task.deadline)
-                            put("is_encrypted", task.isEncrypted)
-                            put("last_modified_time", task.lastModifiedTime)
-                        })
+        ActivityResultContracts.StartActivityForResult(),
+        object : ActivityResultCallback<ActivityResult> {
+            override fun onActivityResult(result: ActivityResult) {
+                val context = requireContext()
+                val data = result.data?.data
+                if (result.resultCode != Activity.RESULT_OK || data == null)
+                    return
+                try {
+                    context.contentResolver.openOutputStream(data)?.use { outputStream ->
+                        val json = JSONObject()
+                        json.put("version", 1)
+                        val taskList = TaskManager.getTasks()
+                        val taskListJsonArray = JSONArray()
+                        json.put("tasks", taskListJsonArray)
+                        for (task in taskList) {
+                            taskListJsonArray.put(JSONObject().apply {
+                                put("create_time", task.createTime)
+                                put("title", task.title)
+                                put("content", task.content)
+                                put("is_finished", task.isFinished)
+                                put("deadline", task.deadline)
+                                put("is_encrypted", task.isEncrypted)
+                                put("last_modified_time", task.lastModifiedTime)
+                            })
+                        }
+                        outputStream.write(json.toString().toByteArray())
                     }
-                    outputStream.write(json.toString().toByteArray())
+                } catch (e: Exception) {
+                    LogUtil.error(getString(R.string.save_data_failed), e)
+                    ToastUtil.toastLong(context, R.string.save_data_failed)
                 }
-            } catch (e: Exception) {
-                LogUtil.error(getString(R.string.save_data_failed), e)
-                ToastUtil.toastLong(context,R.string.save_data_failed)
             }
-        } else {
-            ToastUtil.toastShort(context,R.string.cancelled)
+
         }
-    }
+    )
     private val mImportDataLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult(),
         object : ActivityResultCallback<ActivityResult> {
             override fun onActivityResult(result: ActivityResult) {
                 val context = requireContext()
-                val data=result.data?.data
-                if (result.resultCode != -1 || data == null) {
-                    ToastUtil.toastShort(context,R.string.cancelled)
+                val data = result.data?.data
+                if (result.resultCode != Activity.RESULT_OK || data == null)
                     return
-                }
                 try {
-                    val json=JSONObject(context.contentResolver.openInputStream(data)?.use {inputStream->
-                        InputStreamReader(inputStream).use { reader->
-                            BufferedReader(reader).use {
-                                it.readText()
+                    val json = JSONObject(
+                        context.contentResolver.openInputStream(data)?.use { inputStream ->
+                            InputStreamReader(inputStream).use { reader ->
+                                BufferedReader(reader).use {
+                                    it.readText()
+                                }
                             }
-                        }
-                    }!!)
-                    val taskListJsonArray=json.getJSONArray("tasks")
-                    val taskList= mutableListOf<TaskEntity>()
+                        }!!
+                    )
+                    val taskListJsonArray = json.getJSONArray("tasks")
+                    val taskList = mutableListOf<TaskEntity>()
                     for (i in 0 until taskListJsonArray.length()) {
-                        val taskJson=taskListJsonArray.getJSONObject(i)
-                        taskList.add(TaskEntity(
-                            taskJson.getLong("create_time"),
-                            taskJson.getString("content"),
-                            taskJson.getString("title"),
-                            taskJson.getInt("is_finished"),
-                            taskJson.getLong("deadline"),
-                            taskJson.getLong("last_modified_time"),
-                            taskJson.getInt("is_encrypted")
-                        ))
+                        val taskJson = taskListJsonArray.getJSONObject(i)
+                        taskList.add(
+                            TaskEntity(
+                                taskJson.getLong("create_time"),
+                                taskJson.getString("content"),
+                                taskJson.getString("title"),
+                                taskJson.getInt("is_finished"),
+                                taskJson.getLong("deadline"),
+                                taskJson.getLong("last_modified_time"),
+                                taskJson.getInt("is_encrypted")
+                            )
+                        )
                     }
-                    ImportDataDialog(context, taskList).show()
-                } catch (ex:Exception) {
-                    LogUtil.error(getString(R.string.import_failed),ex)
-                    ToastUtil.toastLong(context,R.string.import_failed)
+                    AlertDialog.Builder(context)
+                        .setCancelable(true)
+                        .setPositiveButton(R.string.append) { _, _ ->
+                            TaskManager.addTasks(taskList)
+                            ToastUtil.toastLong(context, R.string.import_succeed)
+                        }.setNegativeButton(R.string.override) { _, _ ->
+                            TaskManager.clear()
+                            TaskManager.addTasks(taskList)
+                            ToastUtil.toastLong(context, R.string.import_succeed)
+                        }.setOnCancelListener {
+                            ToastUtil.toastShort(context, R.string.cancelled)
+                        }
+                        .setMessage(
+                            String.format(
+                                context.getString(R.string.notice_import_data),
+                                taskList.size
+                            )
+                        )
+                        .show()
+                } catch (ex: Exception) {
+                    LogUtil.error(getString(R.string.import_failed), ex)
+                    ToastUtil.toastLong(context, R.string.import_failed)
                 }
             }
         })
 
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        preferenceManager.sharedPreferencesName="setting"
-        setPreferencesFromResource(R.xml.preferences,rootKey)
+        preferenceManager.sharedPreferencesName = "setting"
+        setPreferencesFromResource(R.xml.preferences, rootKey)
         preferenceManager.findPreference<Preference>("import_data")?.setOnPreferenceClickListener {
             val intent = Intent().apply {
                 setAction("android.intent.action.PICK")
@@ -121,13 +143,14 @@ class SettingFragment : PreferenceFragmentCompat() {
             mExportDataLauncher.launch(intent)
             true
         }
-        preferenceManager.findPreference<SwitchPreference>("deadline_notification")?.setOnPreferenceChangeListener{_,newValue->
-            val intent = Intent(context, CheckDeadlineService::class.java)
-            if (newValue==true)
-                context?.startService(intent)
-            else
-                context?.stopService(intent)
-            true
-        }
+        preferenceManager.findPreference<SwitchPreference>("deadline_notification")
+            ?.setOnPreferenceChangeListener { _, newValue ->
+                val intent = Intent(context, CheckDeadlineService::class.java)
+                if (newValue == true)
+                    context?.startService(intent)
+                else
+                    context?.stopService(intent)
+                true
+            }
     }
 }
